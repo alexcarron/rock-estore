cf435a1e881ac5be579c12d9ec0e92a0909958cb
RJLE <rlemboehms@gmail.com>
2024-02-17 14:50:43 -0500

    Added name attribute

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Rock.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Rock.java
@@ -12,11 +12,12 @@ public class Rock {
     private static final Logger LOG = Logger.getLogger(Rock.class.getName());
 
     // Package private for tests
-    static final String STRING_FORMAT = "Rock [id=%d, price=%f, type=%s, size=%f, shape=%s, description=%s]";
+    static final String STRING_FORMAT = "Rock [id=%d, name=%s, type=%s, price=%f, size=%f, shape=%s, description=%s]";
 
     @JsonProperty("id") private int id;
-    @JsonProperty("price") private int price;
+    @JsonProperty("name") private String name;
     @JsonProperty("type") private String type;
+    @JsonProperty("price") private int price;
     @JsonProperty("size") private double size;
     @JsonProperty("shape") private String shape;
     @JsonProperty("description") private String description;
@@ -24,19 +25,24 @@ public class Rock {
     /**
      * Create a rock with the given id, price, type, size, shape and description
      * @param id The id of the rock
-     * @param price The price of the rock
+     * @param name The name of the rock
      * @param type The type of the rock
+     * @param price The price of the rock
+     * @param size The size of the rock in feet
+     * @param shape The shape of the rock
+     * @param description The description of the rock
      *
      * {@literal @}JsonProperty is used in serialization and deserialization
      * of the JSON object to the Java object in mapping the fields.  If a field
      * is not provided in the JSON object, the Java field gets the default Java
      * value, i.e. 0 for int
      */
-    public Rock(@JsonProperty("id") int id, @JsonProperty("price") int price, @JsonProperty("type") String type,
+    public Rock(@JsonProperty("id") int id, @JsonProperty("name") String name, @JsonProperty("price") int price, @JsonProperty("type") String type,
                 @JsonProperty("size") double size, @JsonProperty("shape") String shape, @JsonProperty("description") String description) {
         this.id = id;
-        this.price = price;
+        this.name = name;
         this.type = type;
+        this.price = price;
         this.size = size;
         this.shape = shape;
         this.description = description;
@@ -52,19 +58,19 @@ public class Rock {
      * Sets the type of the rock - necessary for JSON object to Java object deserialization
      * @param type The type of the rock
      */
-    public void setType(String type) {this.type = type;}
+    public void setName(String name) {this.name = name;}
 
     /**
      * Retrieves the type of the rock
      * @return The type of the rock
      */
-    public String getRockType() {return type;}
+    public String getName() {return name;}
 
     /**
      * {@inheritDoc}
      */
     @Override
     public String toString() {
-        return String.format(STRING_FORMAT,id,price,type,size,shape,description);
+        return String.format(STRING_FORMAT,id,name,type,price,size,shape,description);
     }
 }
