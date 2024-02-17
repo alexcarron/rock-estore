f39f29625a30ea8b3b7f4489c4ce64d425760830
RJLE <rlemboehms@gmail.com>
2024-02-17 14:29:19 -0500

    Added attributes to Rock model

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Rock.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Rock.java
@@ -9,27 +9,37 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @author SWEN Faculty
  */
 public class Rock {
-    private static final Logger LOG = Logger.getLogger(Rock.class.getName());
+    private static final Logger LOG = Logger.getLogger(Rock.class.getRockType());
 
     // Package private for tests
-    static final String STRING_FORMAT = "Rock [id=%d, name=%s]";
+    static final String STRING_FORMAT = "Rock [id=%d, type=%s]";
 
     @JsonProperty("id") private int id;
-    @JsonProperty("name") private String name;
+    @JsonProperty("price") private int price;
+    @JsonProperty("type") private String type;
+    @JsonProperty("size") private double size;
+    @JsonProperty("shape") private String shape;
+    @JsonProperty("description") private String description;
 
     /**
-     * Create a rock with the given id and name
+     * Create a rock with the given id, price, type, size, shape and description
      * @param id The id of the rock
-     * @param name The name of the rock
+     * @param price The price of the rock
+     * @param type The type of the rock
      *
      * {@literal @}JsonProperty is used in serialization and deserialization
      * of the JSON object to the Java object in mapping the fields.  If a field
      * is not provided in the JSON object, the Java field gets the default Java
      * value, i.e. 0 for int
      */
-    public Rock(@JsonProperty("id") int id, @JsonProperty("name") String name) {
+    public Rock(@JsonProperty("id") int id, @JsonProperty("price") int price, @JsonProperty("type") String type,
+                @JsonProperty("size") double size, @JsonProperty("shape") String shape, @JsonProperty("description") String description) {
         this.id = id;
-        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.size = size;
+        this.shape = shape;
+        this.description = description;
     }
 
     /**
@@ -39,22 +49,22 @@ public class Rock {
     public int getId() {return id;}
 
     /**
-     * Sets the name of the rock - necessary for JSON object to Java object deserialization
-     * @param name The name of the rock
+     * Sets the type of the rock - necessary for JSON object to Java object deserialization
+     * @param type The type of the rock
      */
-    public void setName(String name) {this.name = name;}
+    public void setType(String type) {this.type = type;}
 
     /**
-     * Retrieves the name of the rock
-     * @return The name of the rock
+     * Retrieves the type of the rock
+     * @return The type of the rock
      */
-    public String getName() {return name;}
+    public String getRockType() {return type;}
 
     /**
      * {@inheritDoc}
      */
     @Override
     public String toString() {
-        return String.format(STRING_FORMAT,id,name);
+        return String.format(STRING_FORMAT,id,type);
     }
 }
