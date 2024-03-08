00c5a9cb5a05f26516bd8923436b8f4ee20d16cd
alexcarron <thealexcarron@gmail.com>
2024-03-08 17:02:33 -0500

    Implement logout component with mock methods

--- estore-ui/src/app/components/logout/logout.component.ts
+++ estore-ui/src/app/components/logout/logout.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './logout.component.css'
 })
 export class LogoutComponent {
+	// Temporary mock variable to maintain state of user logged in or not
+	isUserLoggedIn: boolean = true;
 
+	constructor(
+		// Should pass service which manages log in state
+	) {
+		// Initialize service here
+	}
+
+	isLoggedIn(): boolean {
+		// TODO: Implement way to check if user is current logged in
+		return this.isUserLoggedIn;
+	}
+
+	logOut() {
+		// TODO: Implement way to logout
+		this.isUserLoggedIn = false;
+		console.log("Logged Out User");
+	}
 }
