de82c05dd180fba105aba794f47734677fd988cb
RJLE <rlemboehms@gmail.com>
2024-03-16 14:21:45 -0400

    Controller-tier unit tests for cart entities

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/CartControllerTest.java
@@ -0,0 +1,80 @@
+package com.estore.api.estoreapi.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import com.estore.api.estoreapi.persistence.CartDAO;
+import com.estore.api.estoreapi.model.Cart;
+import com.estore.api.estoreapi.model.Rock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The unit test suite for the CartController class
+ * 
+ * @author Ryan Lembo-Ehms
+ */
+@Tag("Controller-tier")
+public class CartControllerTest {
+    private CartController cartController;
+    private CartDAO mockCartDao;
+
+    /**
+     * Before each test, create a new Cart Controller object and
+     * inject a mock Cart DAO
+     */
+    @BeforeEach
+    public void setupCartController() {
+        mockCartDao = mock(CartDAO.class);
+        cartController = new CartController(mockCartDao);
+    }
+
+    /**
+     * Test the getItemsFromCart method
+     * @throws IOException 
+     */
+    @Test
+    public void testGetItemsFromCart() throws IOException {
+        // Setup rocks
+        Rock[] expectedRocks = new Rock[2];
+        expectedRocks[0] = new Rock(99, "Wi-Fire",  "igneous", 10, 25, "spherical", "A rock");
+        expectedRocks[1] = new Rock(100, "Galactic Agent",  "igneous", 10, 25, "spherical", "A rock");
+        
+        // Set up cart
+        int testCartId = 10;
+        Cart testCart = new Cart(testCartId, new int[]{99, 100});
+        when(mockCartDao.getCart(testCartId)).thenReturn(testCart);
+        when(mockCartDao.getRocksFromCart(testCart)).thenReturn(expectedRocks);
+
+        // Invoke
+        Rock[] actualRocks = cartController.getItemsFromCart(testCartId).getBody();
+
+        // Assert
+        assertEquals(expectedRocks, actualRocks);
+    }
+
+    /**
+     * Test the getCarts method
+     */
+    @Test
+    public void testGetCarts() throws IOException{
+        // Setup carts
+        Cart[] expected_carts = new Cart[3];
+        expected_carts[0] = new Cart(10, new int[]{1,2,3});
+        expected_carts[1] = new Cart(11, new int[]{6,7,8});
+        expected_carts[2] = new Cart(12, new int[]{99,100,101});
+        when(mockCartDao.getCarts()).thenReturn(expected_carts);
+
+        // Invoke
+        Cart[] actual_carts = cartController.getCarts().getBody();
+
+        // Assert
+        assertEquals(expected_carts, actual_carts);
+    }
+}
--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/CartFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/CartFileDAOTest.java
@@ -1,12 +1,8 @@
 package com.estore.api.estoreapi.persistence;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -16,7 +12,6 @@ import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.estore.api.estoreapi.model.Cart;
 import com.estore.api.estoreapi.model.Rock;
-import com.estore.api.estoreapi.model.User;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -96,7 +91,7 @@ public class CartFileDAOTest {
 
         // Analyze
         assertEquals(carts.length, testCarts.length);
-        
+
         int index = 0;
         for (Cart cart : carts) {
             assertEquals(cart, testCarts[index]);
