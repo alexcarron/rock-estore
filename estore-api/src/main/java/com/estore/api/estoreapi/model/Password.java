f0259c45e84e81934d469d1f5dbe8c958d6920ad
alexcarron <thealexcarron@gmail.com>
2024-03-26 15:23:35 -0400

    Replace validPasword method to always return true until implementation done

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Password.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Password.java
@@ -8,13 +8,14 @@ import java.util.regex.Pattern;
  * @author Samuel Roberts
  */
 public class Password {
-  private static final String validPasswordRegex = 
+  private static final String validPasswordRegex =
   "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";;
   private static final Pattern regexPattern = Pattern.compile(validPasswordRegex);
 
   public static boolean validPassword(String password){
-    Matcher matches = regexPattern.matcher(password);
-    return matches.matches();
+    // Matcher matches = regexPattern.matcher(password);
+    // return matches.matches();
+		return true; // TODO: Replace with implementation
   }
 
   public static String createStrongPassword(String password){
