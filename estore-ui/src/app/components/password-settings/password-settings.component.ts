9fde8f9bf0674bbbeb223b086c8d667aaea69e46
alexcarron <thealexcarron@gmail.com>
2024-03-26 15:05:14 -0400

    Add password-setting component skeleton

---
+++ estore-ui/src/app/components/password-settings/password-settings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-settings',
+  templateUrl: './password-settings.component.html',
+  styleUrl: './password-settings.component.css'
+})
+export class PasswordSettingsComponent {
+
+}
