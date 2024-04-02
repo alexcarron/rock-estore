986d532e875bcf3191709d17589ecf2c353be9ad
alexcarron <thealexcarron@gmail.com>
2024-04-02 16:59:44 -0400

    Add rock images directory with rock images

--- estore-ui/src/app/services/rock/rock.service.ts
+++ estore-ui/src/app/services/rock/rock.service.ts
@@ -13,6 +13,7 @@ export class RockService {
 	httpOptions = {
 		headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 	};
+	private readonly ROCK_IMAGES_DIRECTORY = "media/images/rocks";
 
   constructor(
 		private http: HttpClient,
